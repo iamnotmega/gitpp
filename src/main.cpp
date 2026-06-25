f00908bad44cb40fee0676dc7d5dfc3de7222403
NotMega <iamnotmega@proton.me>
2026-06-25 09:10:38 +0300

    main: add basic command handling and usage text

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,34 @@
 #include <filesystem>
 #include <iostream>
 #include "repository.hpp"
-#include "object.hpp"
 
-// TODO: Implement argument handling for commands
-int main() {
-    std::string name = "gitppTest";
-    gitpp::init(name);
+void print_usage() {
+    std::cout << "Usage: gitpp <command> [<args>]\n\n"
+              << "Available commands:\n"
+              << "  init <directory>           Initialize a new, empty repository\n";
+}
 
-    std::filesystem::current_path(name);
+int main(int argc, char* argv[]) {
+    /* Ensure a subcommand has been passed, otherwise print the usage */
+    if (argc < 2) {
+        print_usage();
+        return 1;
+    }
 
-    std::string test_data = "hello world";
-    std::string generated_hash = hash_object(test_data, "blob", true);
+    std::string command = argv[1];
 
-    std::cout << "Generated Hash: " << generated_hash << std::endl;
+    if (command == "init") {
+        if (argc < 3) {
+            std::cerr << "Error: 'init' requires a target directory name.\n";
+            print_usage();
+            return 1;
+        }
+        std::string repo_name = argv[2];
+        gitpp::init(repo_name); /* Create empty repository with passed name */
+    } else { /* Unknown command handling */
+        std::cerr << "Unknown command: " << command << '\n';
+        print_usage();
+        return 1;
+    }
     return 0;
 }
