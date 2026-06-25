8727159059ab494623a3351673f7e80c3f77d2ff
NotMega <iamnotmega@proton.me>
2026-06-25 18:38:52 +0300

    main: add --help argument for printing usage

--- src/main.cpp
+++ src/main.cpp
@@ -9,26 +9,32 @@ void print_usage() {
               << "  init <directory>           Initialize a new, empty repository\n";
 }
 
-int main(int argc, char* argv[]) {
-    /* Ensure a subcommand has been passed, otherwise print the usage */
+int main(const int argc, char* argv[]) {
+    /* Ensure a subcommand has been passed, otherwise print help message */
     if (argc < 2) {
-        print_usage();
+        std::cout << "gitpp: no command\n";
+        std::cout << "Usage: gitpp <command> [<args>]\n";
+        std::cout << "Try 'gitpp --help' for more options.\n";
         return 1;
     }
 
     std::string command = argv[1];
 
+    if (command == "--help") {
+        print_usage();
+        return 0;
+    }
     if (command == "init") {
         if (argc < 3) {
             std::cerr << "Error: 'init' requires a target directory name.\n";
-            print_usage();
+            std::cout << "Try 'gitpp --help' for more options.\n";
             return 1;
         }
         std::string repo_name = argv[2];
         gitpp::init(repo_name); /* Create empty repository with passed name */
     } else { /* Unknown command handling */
         std::cerr << "Unknown command: " << command << '\n';
-        print_usage();
+        std::cout << "Try 'gitpp --help' for more options.\n";
         return 1;
     }
     return 0;
